6223fd7da5cd5c38014319a5d0af53a1cd9d9b63
Nick <nickfong@2017-041.rz.local>
2025-08-25 20:17:20 -0700

    add REPL on server to pause/resume/quit game

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
@@ -20,21 +21,43 @@ func main() {
 	}
 	fmt.Println("Connection was successful!")
 	defer connection.Close()
+	gamelogic.PrintServerHelp()
 
-	data, err := json.Marshal(routing.PlayingState{IsPaused: true})
-	if err != nil {
-		fmt.Printf("Error marshalling data: %v", err)
-		return
-	}
-	if err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, data); err != nil {
-		fmt.Printf("Error publishing data to exchange: %v", err)
-		return
-	}
+	// REPL
+	for {
+		input := gamelogic.GetInput()
+		if len(input) == 0 {
+			continue
+		}
+		word := input[0]
+		if word == "pause" {
+			fmt.Println("Pausing the game...")
+			if err := ToggleGameState(channel, true); err != nil {
+				fmt.Printf("error: %v", err)
+				os.Exit(1)
+			}
 
-	// wait for ctrl-c
-	signalChan := make(chan os.Signal, 1)
-	signal.Notify(signalChan, os.Interrupt)
-	<-signalChan
+		} else if word == "resume" {
+			fmt.Println("Resuming the game...")
+			if err := ToggleGameState(channel, false); err != nil {
+				fmt.Printf("error: %v", err)
+				os.Exit(1)
+			}
+		} else if word == "quit" {
+			fmt.Println("Exiting the game...")
+			break
+		} else {
+			fmt.Println("Please type a valid command.")
+		}
+	}
 
 	fmt.Println("Shutting down Peril server...")
+}
+
+func ToggleGameState(channel *amqp091.Channel, pause bool) error {
+	data, err := json.Marshal(routing.PlayingState{IsPaused: pause})
+	if err != nil {
+		return err
+	}
+	return pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, data)
 }
